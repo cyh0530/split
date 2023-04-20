8abac622128f9eaa4209cc2ad0ce2156f46b7b55
cyh0530 <chiayanghuang0@gmail.com>
2023-04-19 23:39:44 -0700

    chore: update fake data

--- src/stories/fakes/receipt.ts
+++ src/stories/fakes/receipt.ts
@@ -3,16 +3,13 @@ import { fakeInitialReceiptItems, fakeReceiptItems } from "./receiptItem";
 
 export const fakeInitialReceipt: Receipt = {
     items: fakeInitialReceiptItems,
-    subTotal: 50,
-    tip: 15,
-    tax: 20,
-    totalPrice: 100
+    subTotal: 121.61,
+    tip: 19.34,
+    tax: 11.85,
+    totalPrice: 152.8
 }
 
 export const fakeReceipt: Receipt = {
+    ...fakeInitialReceipt,
     items: fakeReceiptItems,
-    subTotal: 50,
-    tip: 15,
-    tax: 20,
-    totalPrice: 100
 }
--- src/stories/fakes/receiptItem.ts
+++ src/stories/fakes/receiptItem.ts
@@ -2,54 +2,166 @@ import { ReceiptItem } from "../../models/receiptItem";
 
 export const fakeInitialReceiptItems: ReceiptItem[] = [
     {
-        id: "1",
-        name: "Very very long food name that is very long",
+        id: "Bahn, James Bahn",
+        name: "Bahn, James Bahn",
         quantity: 1,
-        unitPrice: 10,
-        totalPrice: 10,
+        unitPrice: 16.95,
+        totalPrice: 16.95,
         buyers: [[]]
     },
     {
-        id: "2",
-        name: "Food 2",
+        id: "Fish & Fries",
+        name: "Fish & Fries",
         quantity: 1,
-        unitPrice: 15,
-        totalPrice: 15,
+        unitPrice: 14.95,
+        totalPrice: 14.95,
         buyers: [[]]
     },
     {
-        id: "3",
-        name: "Food 3",
+        id: "Crispy Fried Chicken",
+        name: "Crispy Fried Chicken",
+        quantity: 1,
+        unitPrice: 15.95,
+        totalPrice: 15.95,
+        buyers: [[]]
+    },
+    {
+        id: "Tots",
+        name: "Tots",
+        quantity: 1,
+        unitPrice: 2,
+        totalPrice: 2,
+        buyers: [[]]
+    },
+    {
+        id: "Shroom & Truffle Shuffle",
+        name: "Shroom & Truffle Shuffle",
+        quantity: 1,
+        unitPrice: 15.95,
+        totalPrice: 15.95,
+        buyers: [[]]
+    },
+    {
+        id: "James West",
+        name: "James West",
         quantity: 2,
-        unitPrice: 12,
-        totalPrice: 24,
+        unitPrice: 16.95,
+        totalPrice: 16.95 * 2,
         buyers: [[], []]
+    },
+    {
+        id: "Caesar Salad",
+        name: "Caesar Salad",
+        quantity: 1,
+        unitPrice: 5.5,
+        totalPrice: 5.5,
+        buyers: [[]]
+    },
+    {
+        id: "Tots-2",
+        name: "Tots",
+        quantity: 1,
+        unitPrice: 3.95,
+        totalPrice: 3.95,
+        buyers: [[]]
+    },
+    {
+        id: "Vanilla Shake",
+        name: "Vanilla Shake",
+        quantity: 1,
+        unitPrice: 7.95,
+        totalPrice: 7.95,
+        buyers: [[]]
+    },
+    {
+        id: "Processing Fee",
+        name: "Processing Fee",
+        quantity: 1,
+        unitPrice: 4.51,
+        totalPrice: 4.51,
+        buyers: [[]]
     }
 ]
 
 export const fakeReceiptItems: ReceiptItem[] = [
     {
-        id: "1",
-        name: "Very very long food name that is very long",
+        id: "Bahn, James Bahn",
+        name: "Bahn, James Bahn",
         quantity: 1,
-        unitPrice: 10,
-        totalPrice: 10,
-        buyers: [["Albert"]]
+        unitPrice: 16.95,
+        totalPrice: 16.95,
+        buyers: [["Angus"]]
     },
     {
-        id: "2",
-        name: "Food 2",
+        id: "Fish & Fries",
+        name: "Fish & Fries",
         quantity: 1,
-        unitPrice: 15,
-        totalPrice: 15,
-        buyers: [["Albert"]]
+        unitPrice: 14.95,
+        totalPrice: 14.95,
+        buyers: [["Angus"]]
     },
     {
-        id: "3",
-        name: "Food 3",
+        id: "Crispy Fried Chicken",
+        name: "Crispy Fried Chicken",
+        quantity: 1,
+        unitPrice: 15.95,
+        totalPrice: 15.95,
+        buyers: [["Justin L."]]
+    },
+    {
+        id: "Tots",
+        name: "Tots",
+        quantity: 1,
+        unitPrice: 2,
+        totalPrice: 2,
+        buyers: [["Justin L."]]
+    },
+    {
+        id: "Shroom & Truffle Shuffle",
+        name: "Shroom & Truffle Shuffle",
+        quantity: 1,
+        unitPrice: 15.95,
+        totalPrice: 15.95,
+        buyers: [["Justin"]]
+    },
+    {
+        id: "James West",
+        name: "James West",
         quantity: 2,
-        unitPrice: 12,
-        totalPrice: 24,
-        buyers: [["Albert"], ["Bob"]]
+        unitPrice: 16.95,
+        totalPrice: 16.95 * 2,
+        buyers: [["Albert"], ["Vincent"]]
+    },
+    {
+        id: "Caesar Salad",
+        name: "Caesar Salad",
+        quantity: 1,
+        unitPrice: 5.5,
+        totalPrice: 5.5,
+        buyers: [["Albert"]]
+    },
+    {
+        id: "Tots-2",
+        name: "Tots",
+        quantity: 1,
+        unitPrice: 3.95,
+        totalPrice: 3.95,
+        buyers: [["Albert"]]
+    },
+    {
+        id: "Vanilla Shake",
+        name: "Vanilla Shake",
+        quantity: 1,
+        unitPrice: 7.95,
+        totalPrice: 7.95,
+        buyers: [["Vincent"]]
+    },
+    {
+        id: "Processing Fee",
+        name: "Processing Fee",
+        quantity: 1,
+        unitPrice: 4.51,
+        totalPrice: 4.51,
+        buyers: [["Albert", "Angus", "Justin", "Justin L.", "Vincent"]]
     }
 ]
