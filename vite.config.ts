8c3fa546d8bbcf139570d7bf1fd60438308c4eb7
cyh0530 <chiayanghuang0@gmail.com>
2023-04-25 21:19:12 -0700

    fix: add base route

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/split/",
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   server: {
     port: 3000,
